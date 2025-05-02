5e8c942e8b67891c3b092c530d89035a31fbfb94
aqueel192@gmail.com <aqueel192@gmail.com>
2025-05-02 21:04:18 +0530

    Concurrent Clients with fork()

--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <netinet/in.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,8 @@
 #define PORT 8000
 
 int main() {
+  signal(SIGCHLD, SIG_IGN);
+
   int server_fd;
   struct sockaddr_in address;
   int addrlen = sizeof(address);
@@ -43,57 +46,64 @@ int main() {
 
   printf("Server is listening on PORT: %d...\n", PORT);
 
-  // Accept a connection
-  client_fd =
-      accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-  if (client_fd < 0) {
-    perror("accept failed");
-    exit(EXIT_FAILURE);
-  }
+  while (1) {
+    // Accept a connection
+    client_fd =
+        accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (client_fd < 0) {
+      perror("accept failed");
+      continue;
+    }
+    // create a child process to handle the client
+    if (fork() == 0) {
+      close(server_fd); // child doesnt need the listener socket
 
-  printf("Client connected!\n");
+      printf("Client connected!\n");
 
-  // Read the request into the buffer
-  int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
-  if (bytes_read > 0) {
-    buffer[bytes_read] = '\0';
-    printf("Recieved request:\n%s\n", buffer);
+      // Read the request into the buffer
+      int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+      if (bytes_read > 0) {
+        buffer[bytes_read] = '\0';
+        printf("Recieved request:\n%s\n", buffer);
 
-    // const char *response = "HTTP/1.1 200 OK\r\n"
-    //                        "Content-Type: text/html\r\n"
-    //                        "Content-Length: 13\r\n"
-    //                        "\r\n"
-    //                        "Hello, world!";
-    //
+        // const char *response = "HTTP/1.1 200 OK\r\n"
+        //                        "Content-Type: text/html\r\n"
+        //                        "Content-Length: 13\r\n"
+        //                        "\r\n"
+        //                        "Hello, world!";
+        //
 
-    FILE *file = fopen("index.html", "r");
-    if (file == NULL) {
-      perror("File not found!");
+        FILE *file = fopen("index.html", "r");
+        if (file == NULL) {
+          perror("File not found!");
+          close(client_fd);
+          continue; // Move to the next request
+        }
+
+        // Read file content into buffer
+        char file_content[8192]; // assuming HTML fits in this size
+        size_t bytes = fread(file_content, 1, sizeof(file_content), file);
+        fclose(file);
+
+        // Build full HTTP response
+        char header[256];
+        sprintf(header,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/html\r\n"
+                "Content-Length: %zu\r\n"
+                "\r\n",
+                bytes);
+        // Send headers first
+        write(client_fd, header, strlen(header));
+        // Send the body (HTML content)
+        write(client_fd, file_content, bytes);
+      }
       close(client_fd);
-      return 1;
+      exit(0); // child exits after handling the request
     }
 
-    // Read file content into buffer
-    char file_content[8192]; // assuming HTML fits in this size
-    size_t bytes = fread(file_content, 1, sizeof(file_content), file);
-    fclose(file);
-
-    // Build full HTTP response
-    char header[256];
-    sprintf(header,
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: %zu\r\n"
-            "\r\n",
-            bytes);
-    // Send headers first
-    write(client_fd, header, strlen(header));
-    // Send the body (HTML content)
-    write(client_fd, file_content, bytes);
+    // close the client connection
+    close(client_fd);
   }
-
-  // close the client connection
-  close(client_fd);
-
   return 0;
 }
